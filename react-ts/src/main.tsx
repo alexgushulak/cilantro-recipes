2ca068606dd05491c074768b52a811b9de968827
Alex Gushulak <alex.gushulak@gmail.com>
2024-05-09 14:59:10 -0400

    (feat) Add react-router

--- react-ts/src/main.tsx
+++ react-ts/src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Root from './routes/root.tsx';
+import ErrorPage from './routes/error-page.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/investors',
+    element: <div>Hello World!</div>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ react-ts/src/routes/error-page.tsx
@@ -0,0 +1,16 @@
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error: unknown = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page" style={{ textAlign: "center", margin: "0 auto"}}>
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{(error as {statusText?: string}).statusText || (error as {message?: string}).message}</i>
+      </p>
+    </div>
+  );
+}
